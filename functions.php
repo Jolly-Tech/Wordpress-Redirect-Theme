fa13ffbb7bd3362ee95822d01dd2641389b012f6
Jan-Ole Busch <janole.busch@gmail.com>
2023-02-15 20:54:08 +0100

    feat: Add menu support

---
+++ functions.php
@@ -0,0 +1,13 @@
+<?php
+
+if (!function_exists('redirect_theme_register_nav_menus')) {
+    function redirect_theme_register_nav_menus()
+    {
+        register_nav_menus([
+            'primary' => __('Primary Menu', 'redirect-theme'),
+            'footer' => __('Footer Menu', 'redirect-theme'),
+        ]);
+    }
+}
+add_action('init', 'redirect_theme_register_nav_menus');
+
